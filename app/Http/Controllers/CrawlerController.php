2a80022c1935c8614d80cbdbaf1e016528a907a3
Theron Smith <theron@thinkstylestudio.com>
2020-09-12 02:35:55 -0600

    add query string param and validation & remove style sheet for now

--- app/Http/Controllers/CrawlerController.php
+++ app/Http/Controllers/CrawlerController.php
@@ -16,14 +16,17 @@ class CrawlerController extends Controller
      */
     public function __invoke(Request $request)
     {
-
-        //- Table to display each page crawled and it's status code
-        $baseUrl = 'https://thinkstylestudio.com';
         $onlyInternal = true;
-        $limit = 30;
+
+        $validatedData = $request->validate(
+            [
+                'baseUrl' => 'required|url|max:255',
+                'limit' => 'required|integer|max:30'
+            ]
+        );
 
         $crawler = new Crawler();
-        $elements = $crawler->retrieveElements($baseUrl, $limit, $onlyInternal);
+        $elements = $crawler->retrieveElements($validatedData['baseUrl'], $validatedData['limit'], $onlyInternal);
         //- Number of a unique images
         $numberOfUniqueImages = $crawler->getNumberOfUniqueImages($elements);
         //- Number of unique internal links
@@ -34,6 +37,7 @@ class CrawlerController extends Controller
         $averagePerPageWordCount = $crawler->averagePerPageWordCount($elements);
         //- Avg Title length
         $averageHeadingLength = $crawler->getAverageHeadingLength($elements);
+
         return view(
             'welcome',
             [
